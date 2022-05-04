07876d76ab38934cb80f68d53ec90210d45ec98e
Max Kim <laika08@naver.com>
2022-05-04 17:30:06 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/activity/MenuActivity.kt
@@ -0,0 +1,17 @@
+package org.techtown.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+    }
+}
